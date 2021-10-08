7ac64839d32d37be0310bc8b0404f82a59a9d793
DESKTOP-EG31S30\yevhe <yevhem.slyva@gmail.com>
2021-10-08 15:19:39 +0300

    user pool

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-central-1_QAiJecJhP', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '13o7kdg9bgf4u57hsc1d7sd0qd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
